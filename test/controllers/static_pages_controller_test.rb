8015eca02995f6d33202bb1cc387fbb8135fbc7c
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2016-10-08 17:35:31 +0000

    Finished layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,29 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title","Twitter"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title","Help | Twitter"
   end
   
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title","About | Twitter"
+  end
+  
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title","Contact | Twitter"
   end
   
 end
